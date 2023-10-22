07a87ec20affd7b19a40f8ebaae61b27ae119b74
Nicholas Laustrup <124007393+nicklaustrup@users.noreply.github.com>
2023-10-22 15:09:31 -0700

    created database generator

--- backend/routes/reviews.py
+++ backend/routes/reviews.py
@@ -1,5 +1,11 @@
+from time import strftime
+
+import dateutil
+
 from flask import Blueprint, jsonify, request, Response, render_template
 from backend.object.Review import ReviewObj as Review
+from faker import Faker
+import random
 
 reviews_bp = Blueprint("reviews", __name__, url_prefix="/reviews")
 
@@ -19,16 +25,9 @@ def get_all_reviews():
         date = review.get_date()
         review_flt = review.get_review()
         review_body = review.get_review_body()
-        json_review_list.append(
-            jsonify(
-                {"id": get_id,
-                 "title": title,
-                 "date": date,
-                 "review": review_flt,
-                 "review_body": review_body}
-            ))
+        json_review_list.append(Review(get_id, title, date, review_flt, review_body))
 
-    return render_template("reviews.html", data=jsonify(json_review_list))
+    return jsonify(Review=[e.serialize() for e in json_review_list])
 
 
 @reviews_bp.route("/create", methods=["POST"])
@@ -48,10 +47,35 @@ def create_reviews():
     return Response(status=204)
 
 
-# def increment_id():
-#     global id_tracker
-#     id_tracker += 1
-#     return id_tracker
-
 def populate_db():
-s
+    global review_database
+
+    fake = Faker()
+
+    # "id": get_id,
+    # "title": title,
+    # "date": date,
+    # "review": review_flt,
+    # "review_body": review_body
+    id_selector = [1, 2, 3, 4, 5, 6, 7, 8]
+    id_to_fair = {1: "Grace Hopper Celebration",
+                  2: "PyCon",
+                  3: "Developer Week",
+                  4: "CodePath",
+                  5: "RTC",
+                  6: "SWE",
+                  7: "TAPIA",
+                  8: "NSBE",
+                  9: "OSU Engineering"}
+
+    for _ in range(0, 15):
+        choice = random.choice(id_selector)
+        fair = id_to_fair[choice]
+        fair_id = choice
+        fair_title = fair
+        date = fake.date_this_year(True, False)
+        date = date.strftime("%m/%d/%y")
+        review = round(random.uniform(4.5, 5.0), 2)
+        review_body = fake.paragraph(2)
+        new_review = Review(fair_id, fair_title, date, review, review_body)
+        review_database.append(new_review)
