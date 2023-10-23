0f3665e232484d60b9abd14947c5349e42145c86
wukongO-O <l.shenglan@gmail.com>
2023-10-22 21:14:15 -0400

    refactored index page code for dynamical rendering

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,17 +7,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-
-@app.route('/fair')
-def fair():
-    return render_template('fair.html')
+    titles = ['Codepath', 'Devloper Week', 'Grace Hoper Celebration',
+              'NSBE', 'OSU Engineering', 'PyCon', 'RTC', 'SWE', 'TAPIA']
+    images = ['static/images/codepath.png', 'static/images/devwk.png', 'static/images/GHC.png', 'static/images/nsbe.png',
+              'static/images/osu.jpeg', 'static/images/pycon.png', 'static/images/rtc.png', 'static/images/swe.png', 'static/images/tapia.png']
+    zipped = zip(titles, images)
+    return render_template('index.html', zipped=zipped)
 
 
 @app.route('/reviews')
 def reviews():
-    return render_template('reviews.html')
+    title = request.args.get('title')
+
+    return render_template('reviews.html', title=title)
 
 
 if __name__ == '__main__':
